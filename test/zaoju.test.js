a582ca484a9703cc0ca5c8115cf823c5109fbfde
谭视怀 Tan Shihuai <tsh20011021@outlook.com>
2018-05-04 19:37:37 +0800

    修正测试用例依赖错误
    其实就自己实现了一点API然后把jQuery删了

--- test/zaoju.test.js
+++ test/zaoju.test.js
@@ -2,20 +2,39 @@ var Zaoju = require('../src/zaoju.js');
 
 var expect = require('chai').expect;
 
-var $ = require('jquery');
+var inArray = function( elem, arr, i ) {
+    var len;
+
+    if ( arr ) {
+
+        len = arr.length;
+        i = i ? i < 0 ? Math.max( 0, len + i ) : i : 0;
+
+		for ( ; i < len; i++ ) {
+
+            // Skip accessing in sparse arrays
+            if ( i in arr && arr[ i ] === elem ) {
+                return i;
+            }
+        }
+    }
+
+    return -1;
+}
+
 
 describe('zaoju.js: ', function() {
     it('regWord("JIM", "my-friend") should be ok.', function() {
         Zaoju.regWord('JIM', 'my-friend');
-		expect($.inArray('JIM', Zaoju.getWordWithTag('my-friend'))).not.to.be.equal(-1);
-	});
-	it('regWord("曾景明", "俺兄弟") should be ok. - Non-ASCII strings', function() {
+        expect(inArray('JIM', Zaoju.getWordWithTag('my-friend'))).not.to.be.equal(-1);
+    });
+    it('regWord("曾景明", "俺兄弟") should be ok. - Non-ASCII strings', function() {
         Zaoju.regWord('曾景明', '俺兄弟');
-		expect($.inArray('曾景明', Zaoju.getWordWithTag('俺兄弟'))).not.to.be.equal(-1);
-	});
-	it('regWord("Louis Victor de Broglie", "Franch physical scientist") should be ok. - Empty spaces', function() {
-    	Zaoju.regWord('Louis Victor de Broglie', 'French physical scientist');
-		expect($.inArray('Louis Victor de Broglie', Zaoju.getWordWithTag('Franch physical scientist'))).not.to.be.equal(-1);
+        expect(inArray('曾景明', Zaoju.getWordWithTag('俺兄弟'))).not.to.be.equal(-1);
+    });
+    it('regWord("Louis Victor de Broglie", "Franch physical scientist") should be ok. - Empty spaces', function() {
+        Zaoju.regWord('Louis Victor de Broglie', 'Franch physical scientist');
+        expect(inArray('Louis Victor de Broglie', Zaoju.getWordWithTag('Franch physical scientist'))).not.to.be.equal(-1);
     
     });
 });
