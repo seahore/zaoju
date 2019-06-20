060b4600432ad1eaaf7740cd77617e16291d3986
谭视怀 Tan Shihuai <tsh20011021@outlook.com>
2019-06-20 18:01:30 +0800

    API增加调整，同时更改测试用例

--- src/zaoju.js
+++ src/zaoju.js
@@ -1,135 +1,198 @@
 (function(){
     
-    var Zaoju = Zaoju || ( function () {
-    
+    var Zaoju = Zaoju || (function() {
         var _dict = {};
-        
-        _dict.time = ["汉武帝时期", "解放前", "上课时", "月黑风高的晚上", "刚才"];
-        _dict.location = ["洞庭湖", "麻将馆", "养生馆", "大街上", "路灯下"];
-        _dict.person = ["你", "班主任", "总统先生", "一位路人", "没有人"];
-        _dict.event = ["搓脚板", "洗澡", "为爱鼓掌", "裸睡", "捡肥皂"];
-   
+        _dict.time=["汉武帝时期","解放前","上课时","月黑风高的晚上","刚才"];
+        _dict.location=["洞庭湖","麻将馆","养生馆","大街上","路灯下"];
+        _dict.person=["你","班主任","总统先生","一位路人","没有人"];
+        _dict.event=["搓脚板","洗澡","为爱鼓掌","裸睡","捡肥皂"];
+        var _pattern = [{
+            type: "tag",
+            value: "time"
+        }, {
+            type: "text",
+            value: "，"
+        }, {
+            type: "tag",
+            value: "person"
+        }, {
+            type: "text",
+            value: "在"
+        }, {
+            type: "tag",
+            value: "location"
+        }, {
+            type: "tag",
+            value: "event"
+        }, {
+            type: "text",
+            value: "。"
+        }, ];
+
         var getDict = function () { return _dict; };
-        
+            
         var setDict = function (d) { _dict = d; };
 
-        var getWordWithTag = function (tag) { return _dict[tag]; };
-
         var clearDict = function () { _dict = {}; };
-    
-        var _pattern = [
-            {type: "tag", value: "time"},
-            {type: "text", value: "，"},
-            {type: "tag", value: "person"},
-            {type: "text", value: "在"},
-            {type: "tag", value: "location"},
-            {type: "tag", value: "event"},
-            {type: "text", value: "。"},
-        ];
         
         var getPattern = function () { return _pattern; };
-    
-        var setPattern = function (p) { _pattern = p; };
-    
-        var regWord = function (val, tags) {
-            if (typeof val !== 'string' || typeof tags !== 'string')
-                throw new TypeError('regWord的参数类型有误');
-            
-            val = val.trim();
         
+        var setPattern = function (p) { _pattern = p; };
+        
+        var getAllDictTags = function() {
+            var tags = [];
+            for(var tag in _dict) {
+                tags.push(tag);
+            }
+            return tags;
+        }
+      
+        var regWord = function(val, tags) {
+            if (typeof val !== "string" || typeof tags !== "string") {
+                throw new TypeError("regWord的参数类型有误");
+            }
+            val = val.trim();
             if (val === "") {
                 throw new Error("诶内容呢？");
                 return;
             }
-            
             tags = tags.trim().split(/[,，、]/);
-            // 去除tags数组内所有的空字符串
             var i = 0;
-            while(i < tags.length) {
-                if(tags[i] === "")
+            while (i < tags.length) {
+                if (tags[i] === "") {
                     tags.splice(i, 1);
-                else
+                } else {
                     ++i;
+                }
             }
-            // 此时i === tags.length
             if (i === 0) {
                 throw new Error("标签有问题呢~");
                 return;
             }
-            
-            // types如果不是数组,转化成数组
-            if (!Array.isArray(tags))
+            if (!Array.isArray(tags)) {
                 tags = [tags];
-            // 将body加入各个标签分类数组
+            }
             for (var i = 0; i < tags.length; ++i) {
-                if (tags[i] === "") continue;
-                if (!(tags[i] in _dict))
+                tags[i] = tags[i].trim();
+                if (tags[i] === "") {
+                    continue;
+                }
+                if (!(tags[i]in _dict)) {
                     _dict[tags[i]] = [];
+                }
                 _dict[tags[i]].push(val);
             }
         };
-    
-        var setPatternInJSON = function (val) {
-            
-            try {
-               var newPattern = JSON.parse(val);
+        var setDictByTag = function(val, tag) {
+            var dst = [];
+
+            if (typeof val !== "string" || typeof tag !== "string") {
+                throw new TypeError("setDictByTag的参数类型有误")
             }
-            catch (e) {
+            val = val.trim();
+            if (val === "") {
+                throw new Error("诶内容呢？");
+                return
+            }
+            tag = tag.trim();
+          
+            var t = "";
+            var isInBrackets = false;
+            for (var i = 0; i < val.length; ++i) {
+                if (val[i] === "[" && isInBrackets === false) {
+                    isInBrackets = true;
+                    continue;
+                }
+                if (val[i] === "]") {
+                    isInBrackets = false;
+                    t = t.trim();
+                    if (t !== "") {
+                        dst.push(t);
+                        t = "";
+                    }
+                    continue;
+                }
+                if (val[i] === "\\") {
+                    t += val[i + 1];
+                    ++i;
+                    continue;
+                }
+                if (isInBrackets) {
+                    t += val[i];
+                }
+            }
+            _dict[tag] = dst;
+        }
+        var getDictByTag = function(tag) {
+            if (typeof tag !== "string") {
+                throw new TypeError("getDictByTag的参数类型有误");
+            }
+                tag = tag.trim();
+            return _dict[tag];
+        }
+        var setPatternInJSON = function(val) {
+            try {
+                var newPattern = JSON.parse(val)
+            } catch (e) {
                 throw new Error("语法似乎不太对呢~");
                 return;
             }
-        
             if (!Array.isArray(newPattern)) {
                 newPattern = [newPattern];
             }
-        
             try {
                 for (var i = 0; i < newPattern.length; ++i) {
-                    if (!("type" in newPattern[i]) || !("value" in newPattern[i]))
+                    if (!("type"in newPattern[i]) || !("value"in newPattern[i])) {
                         throw new Error("检查第" + i.toString() + "个元素中type或value的拼写");
-                    else if (typeof newPattern[i].type !== "string" || newPattern[i].type === "" || typeof newPattern[i].value !== "string" || newPattern[i].value === "")
-                        throw new Error("第" + i.toString() + "个元素的属性值须为非空字符串");
+                    } else {
+                        if (typeof newPattern[i].type !== "string" || newPattern[i].type === "" || typeof newPattern[i].value !== "string" || newPattern[i].value === "") {
+                            throw new Error("第" + i.toString() + "个元素的属性值须为非空字符串");
+                        }
+                    }
                 }
-            }
-            catch (e) {
+            } catch (e) {
                 throw e;
                 return;
             }
-            
             _pattern = newPattern;
         };
-        
-        var setPatternInSPN = function (val) {
-            if (typeof val !== 'string')
-                throw new Error('setPatternInSPN 参数须为字符串');
-            
+        var setPatternInSPN = function(val) {
+            if (typeof val !== "string") {
+                throw new Error("setPatternInSPN 参数须为字符串");
+            }
             var newPattern = [];
-            
             val = val.trim();
-            
-            // 解析
-            var curElement = {type:"text", value: ""};
+            var curElement = {
+                type: "text",
+                value: ""
+            };
             for (var i = 0; i < val.length; ++i) {
-                if (val[i] === '[') {
+                if (val[i] === "[") {
                     if (curElement.type === "tag") {
-                        curElement.value += '[';
+                        curElement.value += "[";
                         continue;
                     }
                     newPattern.push(curElement);
-                    curElement = {type: "tag", value: ""};
+                    curElement = {
+                        type: "tag",
+                        value: ""
+                    };
                     continue;
                 }
-                if (val[i] === ']') {
+                if (val[i] === "]") {
                     if (curElement.type === "text") {
-                        curElement.value += ']';
+                        curElement.value += "]";
                         continue;
                     }
                     newPattern.push(curElement);
-                    curElement = {type: "text", value: ""};
+                    curElement = {
+                        type: "text",
+                        value: ""
+                    };
                     continue;
                 }
-                if (val[i] === '\\') {
-                    curElement.value += val[i+1];
+                if (val[i] === "\\") {
+                    curElement.value += val[i + 1];
                     ++i;
                     continue;
                 }
@@ -137,68 +200,68 @@
                 continue;
             }
             newPattern.push(curElement);
-            // 去空串
             var i = 0;
-            while(i < newPattern.length) {
-                if(newPattern[i].value === "")
+            while (i < newPattern.length) {
+                if (newPattern[i].value === "") {
                     newPattern.splice(i, 1);
-                else
+                } else {
                     ++i;
+                }
             }
-            
             _pattern = newPattern;
         };
-        
-        var genSentence = function () {
+        var genSentence = function() {
             var output = "";
             for (var i = 0; i < _pattern.length; ++i) {
                 switch (_pattern[i].type) {
-                    case "text":
-                        output += _pattern[i].value;
-                        break;
-                    case "tag":
-                        var tagArr = _dict[_pattern[i].value];
-                        if(Array.isArray(tagArr))
-                            output += tagArr[Math.floor(Math.random() * tagArr.length)];
-                        break;
+                case "text":
+                    output += _pattern[i].value;
+                    break;
+                case "tag":
+                    var tagArr = _dict[_pattern[i].value];
+                    if (Array.isArray(tagArr)) {
+                        output += tagArr[Math.floor(Math.random() * tagArr.length)];
+                    }
+                    break
                 }
             }
             return output;
         };
+        /*
     
-    /*
-    
-    // 检查两个生成规则(pattern)是否相等，注意：不检查结构是否合法
-    var comparePatterns = function (p1, p2) {
-        if (!Array.isArray(p1) || !Array.isArray(p2))
-            throw new TypeError('arguments of function comparePatterns should be arrays.');
+        // 检查两个生成规则(pattern)是否相等，注意：不检查结构是否合法
+        var comparePatterns = function (p1, p2) {
+            if (!Array.isArray(p1) || !Array.isArray(p2))
+                throw new TypeError('arguments of function comparePatterns should be arrays.');
         
-        if (p1.length !== p2.length)
-            return false;
-        
-        for ( var i = 0; i < p1.length; ++i) {
-            if (p1[i].type !== p2[i].type || p1[i].value !== p2[i].value)
+            if (p1.length !== p2.length)
                 return false;
-        }
         
-        return true;
-    };
+            for ( var i = 0; i < p1.length; ++i) {
+                if (p1[i].type !== p2[i].type || p1[i].value !== p2[i].value)
+                    return false;
+            }
+        
+            return true;
+        };
     
-    */
-    
+        */
         return {
             getDict: getDict,
-            getWordWithTag: getWordWithTag,
-            regWord: regWord,
             setDict: setDict,
             clearDict: clearDict,
             getPattern: getPattern,
             setPattern: setPattern,
+            getAllDictTags: getAllDictTags,
+            getDictByTag: getDictByTag,
+            regWord: regWord,
+            setDictByTag: setDictByTag,
             setPatternInJSON: setPatternInJSON,
             setPatternInSPN: setPatternInSPN,
             genSentence: genSentence,
         };
     }());
+
     
     
     var zaoju = Zaoju;
--- test/zaoju.test.js
+++ test/zaoju.test.js
@@ -29,16 +29,70 @@ var inArray = function( elem, arr, i ) {
 describe('regWord: ', function() {
     it('regWord("JIM", "my-friend") should be ok.', function() {
         Zaoju.regWord('JIM', 'my-friend');
-        expect(inArray('JIM', Zaoju.getWordWithTag('my-friend'))).not.to.equal(-1);
+        expect(inArray('JIM', Zaoju.getDictByTag('my-friend'))).not.to.equal(-1);
     });
     it('regWord("曾景明", "俺兄弟") should be ok. - Non-ASCII strings', function() {
         Zaoju.regWord('曾景明', '俺兄弟');
-        expect(inArray('曾景明', Zaoju.getWordWithTag('俺兄弟'))).not.to.equal(-1);
+        expect(inArray('曾景明', Zaoju.getDictByTag('俺兄弟'))).not.to.equal(-1);
     });
     it('regWord("Louis Victor de Broglie", "French physical scientist") should be ok. - Empty spaces', function() {
         Zaoju.regWord('Louis Victor de Broglie', 'French physical scientist');
-        expect(inArray('Louis Victor de Broglie', Zaoju.getWordWithTag('French physical scientist'))).not.to.equal(-1);
-    
+        expect(inArray('Louis Victor de Broglie', Zaoju.getDictByTag('French physical scientist'))).not.to.equal(-1);
+    });
+    it('regWord("Amiya", "hare,donkey"), should be ok. - Multi-tag word', function() {
+        Zaoju.regWord("Amiya", "hare,donkey");
+        expect(inArray('Amiya', Zaoju.getDictByTag('hare')) && inArray('Amiya', Zaoju.getDictByTag('donkey'))).not.to.equal(-1);
+    });
+    it('regWord("  Cai Xukun  ", "nba   ,  basketball player,   kun  "), should be ok. - Trim test', function() {
+        Zaoju.regWord("  Cai Xukun  ", "nba   ,  basketball player,   kun  ");
+        expect(inArray('Cai Xukun', Zaoju.getDictByTag('nba')) && inArray('Cai Xukun', Zaoju.getDictByTag('basketball player')) && inArray('Cai Xukun', Zaoju.getDictByTag('kun'))).not.to.equal(-1);
+    });
+});
+
+describe('setDictByTag: ', function() {
+	var d1 = {time: ["新石器时代", "在半封建半殖民地社会时期", "翘课后", "月黑风高的晚上", "刚才", "明天", "9102年", "危机纪元12年", "文艺复兴时期", "此时此刻", "趁不注意"]};
+	var d2 = d1;
+	var d3 = {time: ["你", "你和哲♂学家", "\\"]};
+	var d4 = {time: ["[", "[["]};
+	var d5 = {time: []};
+	var d6 = {time: ["that's right"]};
+	var d7 = d6;
+	
+	var l1 = "[你][你和哲♂学家][谁][一位路人][没有人][你，没错，就是你！你][NBA首位新春贺岁形象大使][游乐娃子][作者自己][诸葛村夫]";
+	var l2 = "233[你]233[你和哲♂学家]233[谁]233[一位路人]233[没有人]233[你，没错，就是你！你]233[NBA首位新春贺岁形象大使]233[游乐娃子]233[作者自己]233[诸葛村夫]";
+	var l3 = "[\\你][你和\\哲\\♂学家][\\\\]";
+	var l4 = "我是乱打的：[[]][[[]]]";
+	var l5 = "我是还乱打的：[][][][][][][][]";
+	var l6 = "【全角括号不行的】[that's right]";
+	var l7 = "[     that's right ]";
+	
+	it('dict line 1 - valid, should be accepted and behave as expected', function() {
+        Zaoju.setDictByTag(l1, "time");
+        expect(Zaoju.getDict()).to.deep.equal(d1);
+    });
+	it('dict line 2 - valid, valid, with characters outside brackets, should be accepted and behave as expected', function() {
+        Zaoju.setDictByTag(l2, "time");
+        expect(Zaoju.getDict()).to.deep.equal(d2);
+    });
+	it('dict line 3 - valid, contains escape chars, should be accepted and behave as expected', function() {
+        Zaoju.setPatternInSPN(l3, "time");
+        expect(Zaoju.getPattern()).to.deep.equal(d3);
+    });
+	it('dict line 4 - valid, contains nested brackets, should be accepted and behave as expected', function() {
+        Zaoju.setPatternInSPN(l4, "time");
+        expect(Zaoju.getPattern()).to.deep.equal(d4);
+    });
+	it('dict line 5 - valid, contains empty tags, should be accepted and behave as expected', function() {
+        Zaoju.setPatternInSPN(l5, "time");
+        expect(Zaoju.getPattern()).to.deep.equal(d5);
+    });
+	it('dict line 6 - valid, uses full-width bracket, should be accepted and behave as expected', function() {
+        Zaoju.setPatternInSPN(l6, "time");
+        expect(Zaoju.getPattern()).to.deep.equal(d6);
+    });});
+	it('dict line 7 - valid, trimming test, should be accepted and behave as expected', function() {
+        Zaoju.setPatternInSPN(l7, "time");
+        expect(Zaoju.getPattern()).to.deep.equal(d7);
     });
 });
 
@@ -58,7 +112,7 @@ describe('setPatternInJSON: ', function() {
     var j4 = '[{"epyt": "text", "value": "foo"}]';
     var j5 = '[{"type": "", "value": "foo"}]';
     
-    it('JSON pattern 1 - valid, should be accepted and be as expected', function() {
+    it('JSON pattern 1 - valid, should be accepted and behave as expected', function() {
         Zaoju.setPattern(d);
         Zaoju.setPatternInJSON(j1);
         expect(Zaoju.getPattern()).to.deep.equal(JSON.parse(j1));
@@ -74,7 +128,7 @@ describe('setPatternInJSON: ', function() {
         }
         throw new Error('No error was throwed when parsing JSON pattern 2.');
     });
-    it('JSON pattern 3 - not an array, should be accepted and be as expected', function() {
+    it('JSON pattern 3 - not an array, should be accepted and behave as expected', function() {
         Zaoju.setPattern(d);
         Zaoju.setPatternInJSON(j3);
         expect(Zaoju.getPattern()).to.deep.equal( [ JSON.parse(j3) ] );
@@ -110,32 +164,44 @@ describe('setPatternInSPN: ', function() {
 	var s3 = '\\嘿，[per\\s\\o\\n]，要使用\\t\\a\\g，只要这样：\\[tag-name\\]。';
     var s4 = '我是乱打的：[[]]';
 	var s5 = '我还是乱打的：[][][]';
+	var s6 = "【全角括号不能括标签】[that's right]";
+	var s7 = "[     that's right   ]";
 	
 	var p1 = [{type: "text", value: "哎呀"}, {type: "tag", value: "person"}, {type: "text", value: "，你不要在"}, {type: "tag", value: "location"}, {type: "text", value: "脱裤子啊......"}];
 	var p2 = [{type: "tag", value: "person"}, {type: "text", value: "在"}, {type: "tag", value: "location"}];
 	var p3 = [{type: "text", value: "嘿，"}, {type: "tag", value: "person"}, {type: "text", value: "，要使用tag，只要这样：[tag-name]。"}];
 	var p4 = [{type: "text", value: "我是乱打的："}, {type: "tag", value: "["}, {type: "text", value: "]"}];
 	var p5 = [{type: "text", value: "我还是乱打的："}];
+	var p6 = [{type: "text", value: "【全角括号不能括标签】"}, {type: "tag", value: "that's right"}];
+	var p7 = [{type: "tag", value: "that's right"}];
 	
-    it('SPN pattern 1 - valid, should be accepted and be as expected', function() {
+    it('SPN pattern 1 - valid, should be accepted and behave as expected', function() {
         Zaoju.setPatternInSPN(s1);
         expect(Zaoju.getPattern()).to.deep.equal(p1);
     });
-	it("SPN pattern 2 - valid, start with '[', should be accepted and be as expected", function() {
+	it("SPN pattern 2 - valid, start with '[', should be accepted and behave as expected", function() {
         Zaoju.setPatternInSPN(s2);
         expect(Zaoju.getPattern()).to.deep.equal(p2);
     });
-	it("SPN pattern 3 - valid, contains escape chars, should be accepted and be as expected", function() {
+	it("SPN pattern 3 - valid, contains escape chars, should be accepted and behave as expected", function() {
         Zaoju.setPatternInSPN(s3);
         expect(Zaoju.getPattern()).to.deep.equal(p3);
     });
-	it("SPN pattern 4 - valid, contains nested brackets, should be accepted and be as expected", function() {
+	it("SPN pattern 4 - valid, contains nested brackets, should be accepted and behave as expected", function() {
         Zaoju.setPatternInSPN(s4);
         expect(Zaoju.getPattern()).to.deep.equal(p4);
     });
-	it("SPN pattern 5 - valid, contains empty tags, should be accepted and be as expected", function() {
+	it("SPN pattern 5 - valid, contains empty tags, should be accepted and behave as expected", function() {
         Zaoju.setPatternInSPN(s5);
         expect(Zaoju.getPattern()).to.deep.equal(p5);
+    });
+	it('SPN pattern 6 - valid, uses full-width bracket, should be accepted and behave as expected', function() {
+        Zaoju.setPatternInSPN(s6);
+        expect(Zaoju.getPattern()).to.deep.equal(p6);
+    });});
+	it('SPN pattern 7 - valid, trimming test, should be accepted and behave as expected', function() {
+        Zaoju.setPatternInSPN(s7);
+        expect(Zaoju.getPattern()).to.deep.equal(p7);
     });
 });
     
